082b394d7c87d3655232e30b976712f2e835c300
Bernardo-MG <programming@bernardo-mg.com>
2023-02-20 09:34:18 +0100

    Cleaned up code

--- src/main/java/com/bernardomg/example/webflux/domain/configuration/FunctionalWebConfig.java
+++ src/main/java/com/bernardomg/example/webflux/domain/configuration/FunctionalWebConfig.java
@@ -4,13 +4,18 @@ package com.bernardomg.example.webflux.domain.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicate;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.bernardomg.example.webflux.domain.model.ExampleEntity;
 import com.bernardomg.example.webflux.domain.service.ExampleEntityService;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class FunctionalWebConfig {
@@ -27,8 +32,18 @@ public class FunctionalWebConfig {
 
     @Bean
     public RouterFunction<ServerResponse> getExampleEntities() {
-        return RouterFunctions.route(RequestPredicates.GET("/functional/entity"), req -> ServerResponse.ok()
-            .body(service.getAll(), ExampleEntity.class));
+        final RequestPredicate route;
+
+        route = RequestPredicates.GET("/functional/entity")
+            .and(RequestPredicates.accept(MediaType.APPLICATION_JSON));
+
+        return RouterFunctions.route(route, this::getAllUsers);
+    }
+
+    private final Mono<ServerResponse> getAllUsers(final ServerRequest request) {
+        return ServerResponse.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(service.getAll(), ExampleEntity.class);
     }
 
 }
